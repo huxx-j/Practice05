76e65fc48c67ab2fce8ec31fa0ae13508f540504
Huxx-j(장성우) <perfekta213@gmail.com>
2018-03-16 17:56:43 +0900

    1st

--- src/com/javaex/problem01/CalcApp.java
+++ src/com/javaex/problem01/CalcApp.java
@@ -14,6 +14,10 @@ public class CalcApp {
         System.out.print(">> ");
         String num = scanner.nextLine();
 
+        String  numArray[] = num.split(" ");
+        if (numArray[1] == "+") {
+//            add.calculate(numArray[0], numArray[2])
+        }
         //nextLine으로 입력 받고  공백으로 숫자 연산자 숫자 분리후 배열저장하고 두번째 배열에 연산자를 if문 돌려서 메소드 호출
     }
 }
