845d93b9f6f5c20bf06a28dedabeff624fe1df65
Huxx-j(장성우) <perfekta213@gmail.com>
2018-03-16 17:29:07 +0900

    1st

--- src/com/javaex/problem02/FriendApp.java
+++ src/com/javaex/problem02/FriendApp.java
@@ -1,9 +1,5 @@
 package com.javaex.problem02;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class FriendApp {
 
     public static void main(String[] args) {
