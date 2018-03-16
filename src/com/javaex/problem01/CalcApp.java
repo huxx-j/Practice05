66eaec4bd13af7982fde4a08480dc381527b244d
Huxx-j(장성우) <perfekta213@gmail.com>
2018-03-16 17:49:24 +0900

    1st

--- src/com/javaex/problem01/CalcApp.java
+++ src/com/javaex/problem01/CalcApp.java
@@ -1,8 +1,19 @@
 package com.javaex.problem01;
 
+import java.util.Scanner;
+
 public class CalcApp {
 
     public static void main(String[] args) {
+        Add add = new Add();
+        Div div = new Div();
+        Sub sub = new Sub();
+        Mul mul = new Mul();
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(">> ");
+        String num = scanner.nextLine();
+
+        //nextLine으로 입력 받고  공백으로 숫자 연산자 숫자 분리후 배열저장하고 두번째 배열에 연산자를 if문 돌려서 메소드 호출
     }
 }
